76ffebc8c11125c73d1956516061a1e9484eb136
mithileshtipkari <mithilesh.tipkari@gmail.com>
2019-08-08 00:24:41 +0530

    Added Campaign contract initial stage

---
+++ Campaign.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.17;
+
+contract Campaign{
+  address public manager;
+  uint public minimumContribution;
+  address[] public approvers;
+
+  function Campaign(uint minContri) public{
+      manager = msg.sender;
+      minimumContribution = minContri;
+  }
+  
+  function contribute() public payable{
+      require(msg.value > minimumContribution);
+      approvers.push(msg.sender);
+  }
+}
