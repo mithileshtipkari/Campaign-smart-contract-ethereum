dfd2f525008c6ca4c7be123e4e3d10526bdc2112
mithileshtipkari <mithilesh.tipkari@gmail.com>
2019-09-12 00:26:04 +0530

    Changed Request struct to hold mapping of approvals and count of positive votes for a request

--- Campaign.sol
+++ Campaign.sol
@@ -1,41 +1,44 @@
 pragma solidity ^0.4.17;
 
 contract Campaign{
-  struct Request{
+    struct Request{
         string description;
         uint value;
         address recipient;
         bool isComplete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
     }
-  address public manager;
-  uint public minimumContribution;
-  mapping(address => bool) public approvers;
 
-  modifier restricted(){
-     require(msg.sender == manager);
-     _;
-  }
-  function Campaign(uint minContri) public{
-      manager = msg.sender;
-      minimumContribution = minContri;
-  }
+    Request[] requests;
+    address public manager;
+    uint public minimumContribution;
+    mapping(address => bool) public approvers;
 
-  function contribute() public payable{
-      require(msg.value > minimumContribution);
-      approvers[msg.sender] = true;
-  }
+    modifier restricted(){
+        require(msg.sender == manager);
+        _;
+    }
 
-  function createRequest(string description, uint value, address recipient) public restricted{
+    function Campaign(uint minContri) public{
+        manager = msg.sender;
+        minimumContribution = minContri;
+    }
+
+    function contribute() public payable{
+        require(msg.value > minimumContribution);
+        approvers[msg.sender] = true;
+    }
+
+    function createRequest(string description, uint value, address recipient) public restricted{
+
         Request memory req = Request({
             description: description,
             value: value,
             recipient: recipient,
-            isComplete: false
+            isComplete: false,
+            approvalCount: 0
         });
         requests.push(req);
-    }
-
-    function getRequestAtIndex(uint index) public restricted view returns(Request){
-        return requests[index];
     }
 }
