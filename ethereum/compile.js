9eaa98fc96cd1dd1bfb51afe5ff29456ffe0771e
mithileshtipkari <mithilesh.tipkari@gmail.com>
2019-09-23 14:24:48 +0530

    Added compilation code and saving compilation output to a json file

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -1,26 +1,28 @@
-const fs = require('fs-extra');
+const fse = require('fs-extra');
+const fs = require('fs');
 const path = require('path');
 const solc = require('solc');
 
-const buildPath =  path.resolve(__dirname, 'build');
-fs.removeSync(buildPath);
+let buildPath =  path.resolve(__dirname, 'build');
+fse.removeSync(buildPath);
 
 const contractPath = path.resolve(__dirname,'contracts','Campaign.sol');
 // console.log('contractPath-', contractPath);
 
-const source = fs.readFileSync(contractPath, 'utf8');
+const source = fse.readFileSync(contractPath, 'utf8');
 // console.log('s -', source);
 
 //compile the contract
 const compiled = solc.compile(source, 1).contracts;
 console.log('compile - ', compiled);
 
-console.log('make');
-fs.ensureDirSync(buildPath);
-console.log('made');
+fse.ensureDirSync(buildPath);
 
 for(let contract in compiled){
   console.log('contract -', contract);
-
-  fs.outputJsonSync(path.resolve(buildPath, contract + '.json'), compiled[contract]);
+  let contractName = contract.substr(1);
+  fse.outputJsonSync(
+    path.resolve(buildPath, contractName + '.json'),
+    compiled[contract]
+  );
 }
