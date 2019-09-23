13f152c618600475130a39fbe21e0d26452ca09d
mithileshtipkari <mithilesh.tipkari@gmail.com>
2019-09-23 16:17:59 +0530

    Fetching accounts with web3 module for testing

---
+++ test/Campaign.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+const compiledCampaignFactory = require('../ethereum/build/CampaignFactory.json');
+const compiledCampaign = require('../ethereum/build/Campaign.json');
+
+let accounts;
+let factory;
+let campaignAddress;
+let campaign;
+
+beforeEach( async () => {
+  accounts = await web3.eth.getAccounts();
+  console.log('acc-', accounts[0]);
+});
