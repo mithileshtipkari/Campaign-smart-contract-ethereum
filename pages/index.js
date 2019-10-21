f223c4df420222f6f0dc087b9968a56c76930b50
mithileshtipkari <mithilesh.tipkari@gmail.com>
2019-10-21 15:23:33 +0530

    Added list of existing campaigns and a button to add a new campaign along with CSS

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Card, Button, Icon } from 'semantic-ui-react';
 import factory from '../ethereum/factory';
 
 class CampaignIndex extends Component{
@@ -7,9 +8,32 @@ class CampaignIndex extends Component{
     return { campaigns} ;
   }
 
+  renderCards(){
+    const items = this.props.campaigns.map(address => {
+      return {
+        header : address,
+        description : <a>View details</a>,
+        fluid : true
+      }
+    });
+
+    return <Card.Group items={items} />;
+  }
+
+  campaignAddButton(){
+    return (
+      <Button content="Create a Campaign" icon="plus" labelPosition="left" primary/>
+    );
+  }
+
   render(){
     return(
-      <div>The deployed Campaign address - {this.props.campaigns}</div>
+        <div>
+          <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
+          <h3>Open Campaigns</h3>
+          {this.renderCards()}
+          {this.campaignAddButton()}
+        </div>
     );
   }
 }
