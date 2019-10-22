3d117166d1dcc0fb46a55708422a7a3d9d57b3b9
mithileshtipkari <mithilesh.tipkari@gmail.com>
2019-10-23 00:23:34 +0530

    Added a form to create a new campaign by asigning minimum contribution

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
+import { Form, Button } from 'semantic-ui-react';
 
 class CampaignNew extends Component{
   render(){
     return(
         <Layout>
-          <h1>New</h1>
+          <h3>Create a new Campaign!</h3>
+          <Form>
+            <Form.Field>
+              <label>Minimum Contribution</label>
+              <input placeholder='Minimum Contribution in Wei' />
+            </Form.Field>
+            <Button primary type='submit'>Create</Button>
+          </Form>
         </Layout>
     )
   }
