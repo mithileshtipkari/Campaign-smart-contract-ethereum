d598fbd391c76ff2938234aa6490975fb0c65096
mithileshtipkari <mithilesh.tipkari@gmail.com>
2019-10-22 23:57:56 +0530

    Added header to all the pages via layout and created New Campaign route

--- pages/index.js
+++ pages/index.js
@@ -32,11 +32,9 @@ class CampaignIndex extends Component{
     return(
       <Layout>
         <div>
-          <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
-          <h3>Open Campaigns</h3>
+          <h3 style={{marginTop:'10px'}}>Open Campaigns</h3>
           {this.campaignAddButton()}
           {this.renderCards()}
-
         </div>
       </Layout>
     );
--- pages/newcampaign.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default () => {
-  return(
-    <div>welcome to newcampaign page</div>
-
-    // <a href='/show'>show page</a>
-  );
-}
