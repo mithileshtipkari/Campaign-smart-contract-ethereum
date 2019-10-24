1b56aa7e0ba12fbf85f1def881efdf4e2681dfdb
mithileshtipkari <mithilesh.tipkari@gmail.com>
2019-10-25 00:19:21 +0530

    UI changes in form

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
-import { Form, Button } from 'semantic-ui-react';
+import { Form, Button, Input } from 'semantic-ui-react';
 
 class CampaignNew extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      minimumContribution : ''
+    }
+  }
   render(){
     return(
         <Layout>
           <h3>Create a new Campaign!</h3>
-          <Form>
-            <Form.Field>
-              <label>Minimum Contribution</label>
-              <input placeholder='Minimum Contribution in Wei' />
-            </Form.Field>
-            <Button primary type='submit'>Create</Button>
-          </Form>
+            <Form>
+              <Form.Field>
+                <label>Minimum Contribution</label>
+                <Input label='wei'
+                        labelPosition='right'
+                        placeholder='Minimum Contribution in Wei'
+                        value={this.state.minimumContribution}
+                        onChange={event => {this.setState({ minimumContribution : event.target.value})}}
+                        />
+              </Form.Field>
+              <Button primary type='submit'>Create</Button>
+            </Form>
         </Layout>
     )
   }
