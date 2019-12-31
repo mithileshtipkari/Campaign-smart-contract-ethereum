30781a93ddb9b61ed15f99f0e81470a2359f5f1e
mithileshtipkari <mithilesh.tipkari@gmail.com>
2019-12-31 16:11:47 +0530

    Added Campaign Information class

---
+++ ethereum/CampaignInformation.js
@@ -0,0 +1,10 @@
+class CampaignInformation{
+  constructor(name, description, minimumContribution, deployedCampaignAddress){
+    this.name = name;
+    this.description = description;
+    this.minimumContribution = minimumContribution;
+    this.deployedCampaignAddress = deployedCampaignAddress;
+  }
+}
+
+export default CampaignInformation;
