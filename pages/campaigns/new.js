a4e9ec916e309fa40bf90ea5a1df51bc8ca39623
mithileshtipkari <mithilesh.tipkari@gmail.com>
2020-01-02 00:24:50 +0530

    handling error for campaign creation form

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
-import { Form, Button, Input } from 'semantic-ui-react';
+import { Form, Button, Input, Message } from 'semantic-ui-react';
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
 
@@ -10,30 +10,37 @@ class CampaignNew extends Component{
     this.state = {
       minimumContribution : '',
       name: '',
-      description: ''
+      description: '',
+      errorMessage: ''
     }
   }
 
   onSubmit = async (event) => {
-    event.preventDefault();
-    console.log('here');
-    console.log('contri-', this.state.minimumContribution);
-    console.log('name -', this.state.name);
-    console.log('desc-', this.state.description);
-    const accounts = await web3.eth.getAccounts();
-    console.log('acc- ', accounts);
-    factory.methods
-      .createCampaign(this.state.minimumContribution, this.state.name, this.state.description)
-      .send({
-        from : accounts[0]
-      });
+    try{
+
+      event.preventDefault();
+      console.log('here');
+      console.log('contri-', this.state.minimumContribution);
+      console.log('name -', this.state.name);
+      console.log('desc-', this.state.description);
+      const accounts = await web3.eth.getAccounts();
+      console.log('acc- ', accounts);
+      factory.methods
+        .createCampaign(this.state.minimumContribution, this.state.name, this.state.description)
+        .send({
+          from : accounts[0]
+        });
+    } catch (err){
+      console.log('wrong - ', err.message);
+      this.setState({ errorMessage: err.message});
+    }
   }
 
   render(){
     return(
         <Layout>
           <h3>Create a new Campaign!</h3>
-            <Form onSubmit={this.onSubmit}>
+            <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
               <Form.Field>
                 <label>Minimum Contribution</label>
                 <Input label='wei'
@@ -44,7 +51,7 @@ class CampaignNew extends Component{
                         />
 
                 <Input label='Name of Contract'
-                      
+
                         labelPosition='right'
                         placeholder="Title of the contract"
                         value={this.state.name}
@@ -58,6 +65,7 @@ class CampaignNew extends Component{
                         onChange={event => {this.setState({description:event.target.value})}}
                         />
               </Form.Field>
+              <Message error header="Oops some error occurred!" list={[this.state.errorMessage]} />
               <Button primary type='submit'>Create</Button>
             </Form>
         </Layout>
