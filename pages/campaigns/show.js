cd4cb24646076b5f62046e5cb0efcec4dfddb402
mithileshtipkari <mithilesh.tipkari@gmail.com>
2020-01-02 16:52:41 +0530

    Added route for campaign information page

---
+++ pages/campaigns/show.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Layout from '../../components/Layout';
+class ShowCampaign extends Component{
+  render(){
+    return (
+      <Layout>
+        <h3>Campaign Info</h3>
+      </Layout>
+    )
+  }
+}
+
+export default ShowCampaign;
