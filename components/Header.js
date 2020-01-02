81fd3eb82c886d1e7f731ad80bf5aa1ae5996396
mithileshtipkari <mithilesh.tipkari@gmail.com>
2020-01-02 16:14:17 +0530

    Added links to homepage and create campaign page

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import { Menu, Header } from 'semantic-ui-react';
+import { Link } from '../routes';
 
 export default () => {
   return(
     <div>
       <Header style={{marginTop:'10px'}} as="h2" icon="money" content="Distributed Crowdfunding Platform" />
       <Menu>
-        <Menu.Item>
-         Distributed Campaign
-        </Menu.Item>
+        <Link route="/">
+          <a className="item">Homepage</a>
+        </Link>
         <Menu.Menu position="right">
-        <Menu.Item>
-         Campaigns
-        </Menu.Item>
-        <Menu.Item>
-         +
-        </Menu.Item>
+          <Link route="/campaigns/new">
+            <a className="item">Create a Campaign</a>
+          </Link>
         </Menu.Menu>
       </Menu>
     </div>
