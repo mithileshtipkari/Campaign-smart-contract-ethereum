6d4c6175ba9fcc68d83b5fd9b5b7f0700999dbd9
mithileshtipkari <mithilesh.tipkari@gmail.com>
2020-01-04 11:43:10 +0530

    Using newly deployed contract

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x0E6658552E9Cc13F396cCCEEE5900F68b0040a9e'
+  '0x647c2b4579F9180d435a189B53EE9665Eef56357'
 );
 
 // console.log('instance ----\n', instance);
