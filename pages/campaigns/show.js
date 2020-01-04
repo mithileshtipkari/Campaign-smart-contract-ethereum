2f292ab2362f5bacd9b81f39739d30c62c5d182e
mithileshtipkari <mithilesh.tipkari@gmail.com>
2020-01-04 12:15:08 +0530

    Calling summary function on campaign

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
+import Campaign from '../../ethereum/campaign';
+
 class ShowCampaign extends Component{
+  static async getInitialProps(props){
+    const campaign = Campaign(props.query.address);
+
+    const summary = await campaign.methods.getSummery().call();//getSummery spelling is wrong
+    console.log('summary -', summary);
+    return {};
+  }
   render(){
     return (
       <Layout>
