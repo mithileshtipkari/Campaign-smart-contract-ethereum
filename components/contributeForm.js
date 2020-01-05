d290e7b39fb8b5271bfa14820db265392b189faf
mithileshtipkari <mithilesh.tipkari@gmail.com>
2020-01-05 14:00:39 +0530

    Added contribute to campaign form

---
+++ components/contributeForm.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Form, Input, Message, Button } from 'semantic-ui-react';
+
+class ContributeForm extends Component{
+  render(){
+    return (
+      <Form>
+        <Form.Field>
+          <label>Amount to contribute</label>
+          <Input
+            label="ether"
+            labelPosition="right"
+          />
+        </Form.Field>
+        <Button primary>Contribute
+        </Button>
+      </Form>
+    );
+  }
+}
+
+export default ContributeForm;
