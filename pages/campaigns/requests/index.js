4d58e7ad5b4f3001ac7392bc8160b6031b9b0d03
mithileshtipkari <mithilesh.tipkari@gmail.com>
2020-01-08 14:26:27 +0530

    Requests page setup

---
+++ pages/campaigns/requests/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Layout from '../../../components/Layout';
+
+class RequestIndex extends Component{
+  render(){
+    return(
+      <div>
+        <Layout>
+          <h3>Hi</h3>
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default RequestIndex;
--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
-import { Header, Icon, Card, Grid } from 'semantic-ui-react';
+import { Header, Icon, Card, Grid, Button } from 'semantic-ui-react';
 import web3 from '../../ethereum/web3';
 import ContributeForm from '../../components/contributeForm';
+import { Link } from '../../routes';
 
 class ShowCampaign extends Component{
   static async getInitialProps(props){
@@ -72,12 +73,23 @@ class ShowCampaign extends Component{
         <hr/>
         {this.renderHeading()}
         <Grid>
-          <Grid.Column width={10}>
+          <Grid.Row>
+            <Grid.Column width={10}>
               {this.renderCards()}
-          </Grid.Column>
-          <Grid.Column width={6}>
-            <ContributeForm address={this.props.address}/>
-          </Grid.Column>
+            </Grid.Column>
+            <Grid.Column width={6}>
+              <ContributeForm address={this.props.address}/>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+              <Link route={`/campaigns/${this.props.address}/requests`}>
+                  <a>
+                    <Button primary>View Requests</Button>
+                  </a>
+              </Link>
+            </Grid.Column>
+          </Grid.Row>
         </Grid>
       </Layout>
     );
