7a69640a243ed9e0409d86a2052c2569b611dbed
mithileshtipkari <mithilesh.tipkari@gmail.com>
2020-01-09 14:35:30 +0530

    Finalizing a request to send money to recipient

--- components/requestRow.js
+++ components/requestRow.js
@@ -11,6 +11,15 @@ class RequestRow extends Component{
       from: accounts[0]
     });
   }
+
+  onFinalize = async () => {
+    const accounts = await web3.eth.getAccounts();
+    const campaign = Campaign(this.props.address);
+    await campaign.methods.finalizeRequest(this.props.id).send({
+      from: accounts[0]
+    });
+  }
+
   render(){
     const { Row, Cell } = Table;
     const { id, request } = this.props;
@@ -23,6 +32,9 @@ class RequestRow extends Component{
         <Cell>{request.approvalCount}/{this.props.contributorsCount}</Cell>
         <Cell>
           <Button basic color="green" onClick={this.onApprove}>Approve</Button>
+        </Cell>
+        <Cell>
+          <Button basic color="teal" onClick={this.onFinalize}>Finalize</Button>
         </Cell>
       </Row>
     );
